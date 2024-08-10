fc9fc76ca7016863d7c3beb61f39e0ace918c405
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-10 06:19:54 +0300

    Add createController.go file

---
+++ quizfy-backend/utils/createController.go
@@ -0,0 +1,39 @@
+package utils
+
+import (
+	"os"
+	"path/filepath"
+)
+
+// CreateFolder creates a folder in the given path.
+
+func CreateFolder(pkgName, path string) error {
+	if err := os.Mkdir(path, 0755); err != nil {
+		return err
+	}
+	createFiles(pkgName, path)
+	return nil
+}
+
+func createFiles(pkgName, path string) {
+	files := []string{"create.go", "get.go", "update.go", "delete.go"}
+	for _, file := range files {
+		fullPath := filepath.Join(path, file)
+
+		// Create or open the file
+		f, err := os.Create(fullPath)
+		if err != nil {
+			panic(err)
+		}
+
+		// Write package declaration
+		if _, err := f.WriteString("package " + pkgName); err != nil {
+			panic(err)
+		}
+
+		// Close the file
+		if err := f.Close(); err != nil {
+			panic(err)
+		}
+	}
+}
