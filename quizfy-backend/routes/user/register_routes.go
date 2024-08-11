2571929aa26cdf9361d2cec3e93f005692a77929
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-11 16:49:21 +0300

    Refactor user registration routes

--- quizfy-backend/routes/user/register_routes.go
+++ quizfy-backend/routes/user/register_routes.go
@@ -5,6 +5,15 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func RegisterRoutes(router *gin.Engine) {
-	router.POST("/api/quizzer", quizzer.Create)
+func RegisterRoutes(api *gin.RouterGroup) {
+	register_routes := api.Group("/register")
+	{
+		// quizzer
+		register_routes.POST("/signup/quizzer", quizzer.Create)
+		register_routes.POST("/login/quizzer", quizzer.Create)
+
+		// student
+		register_routes.POST("/signup/student", quizzer.Create)
+		register_routes.POST("/login/student", quizzer.Create)
+	}
 }
