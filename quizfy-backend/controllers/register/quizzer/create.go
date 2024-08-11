5c243721fdff14a8c517a0e566747522b33ac81f
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-11 16:48:44 +0300

    Refactor quizzer creation function

--- quizfy-backend/controllers/register/quizzer/create.go
+++ quizfy-backend/controllers/register/quizzer/create.go
@@ -1,35 +1,33 @@
 package quizzer
 
 import (
-	"errors"
 	"net/http"
 
-	"github.com/YasserRABIE/QUIZFYv2/db"
+	quizzerM "github.com/YasserRABIE/QUIZFYv2/migrations/register/quizzer_migration"
 	"github.com/YasserRABIE/QUIZFYv2/models/response"
 	"github.com/YasserRABIE/QUIZFYv2/models/user"
 	"github.com/YasserRABIE/QUIZFYv2/services/auth"
 	"github.com/YasserRABIE/QUIZFYv2/utils"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
 func Create(c *gin.Context) {
-	var quizzer user.Quizzer
+	var q user.Quizzer
 
 	// Bind request body
-	err := c.ShouldBindJSON(&quizzer)
+	err := c.ShouldBindJSON(&q)
 	if utils.HandleError(c, err, http.StatusBadRequest) {
 		return
 	}
 
 	// Create quizzer
-	err = createQuizzer(&quizzer)
+	err = quizzerM.Create(&q)
 	if utils.HandleError(c, err, http.StatusBadRequest) {
 		return
 	}
 
 	// Generate token
-	token, err := auth.CreateToken(quizzer.Phone)
+	token, err := auth.CreateToken(q.ID)
 	if utils.HandleError(c, err, http.StatusInternalServerError) {
 		return
 	}
@@ -37,16 +35,4 @@ func Create(c *gin.Context) {
 	// Send success response
 	r := response.NewSuccess(token)
 	c.JSON(http.StatusCreated, r)
-}
-
-func createQuizzer(q *user.Quizzer) error {
-	err := db.Conn.Create(q).Error
-	if err != nil {
-		// Check if the error is due to a unique constraint violation
-		if errors.Is(err, gorm.ErrDuplicatedKey) {
-			return errors.New("الرقم ده مسجل قبل كده")
-		}
-		return errors.New("حصل خطأ في السيرفر. حاول تاني بعد شوية")
-	}
-	return nil
 }
