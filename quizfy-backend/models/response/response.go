c383febfb640e8d5828ada2e6d47ba55031bef7d
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-11 12:34:27 +0300

    Add response package with success and error constructors

---
+++ quizfy-backend/models/response/response.go
@@ -0,0 +1,20 @@
+package response
+
+type Response struct {
+	Success bool        `json:"success"`
+	Data    interface{} `json:"data"`
+}
+
+func NewSucces(data interface{}) *Response {
+	return &Response{
+		Success: true,
+		Data:    data,
+	}
+}
+
+func NewError(err string) *Response {
+	return &Response{
+		Success: false,
+		Data:    err,
+	}
+}
