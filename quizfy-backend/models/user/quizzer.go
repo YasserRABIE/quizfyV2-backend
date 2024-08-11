901cac860a038ab744b64df96f7f0d57dfe8012c
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-11 15:13:39 +0300

    Add binding validation for Subject field

--- quizfy-backend/models/user/quizzer.go
+++ quizfy-backend/models/user/quizzer.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Quizzer struct {
 	gorm.Model
 	UserInfo
-	Subject string `json:"subject" gorm:"not null"`
+	Subject string `json:"subject" gorm:"not null" binding:"required"`
 }
