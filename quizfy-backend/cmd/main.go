93a7baf76408e81acf1214f8578ea16daf1515e7
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-14 16:46:00 +0300

    Add CORS configuration for API

--- quizfy-backend/cmd/main.go
+++ quizfy-backend/cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/YasserRABIE/QUIZFYv2/config"
 	"github.com/YasserRABIE/QUIZFYv2/db"
 	routes "github.com/YasserRABIE/QUIZFYv2/routes/user"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,6 +24,15 @@ func init() {
 
 func main() {
 	r := gin.Default()
+	// CORS configuration
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	api_v1 := r.Group("/api/v1")
 	// Register routes
