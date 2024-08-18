ab6e6624640560a924d1355698307beb0774bfe1
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-18 12:00:31 +0300

    Add create and get quiz functionality

--- quizfy-backend/controllers/quiz/create.go
+++ quizfy-backend/controllers/quiz/create.go
@@ -1,1 +1,39 @@
 package quiz
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/quiz_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Create(c *gin.Context) {
+	var quizData quiz.Quiz
+
+	// add user_id to quizData from token
+	userID, _ := c.Get("user_id")
+	quizData.UserID = userID.(uint)
+
+	// Bind request body
+	err := c.ShouldBindJSON(&quizData)
+	if err != nil {
+		err = errors.New("يرجى ملأ كل الحقول")
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Create quiz in database
+	err = quiz_migrations.Create(&quizData)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Send success response
+	r := response.NewSuccess(quizData)
+	c.JSON(http.StatusCreated, r)
+}
--- quizfy-backend/controllers/quiz/get.go
+++ quizfy-backend/controllers/quiz/get.go
@@ -1,1 +1,23 @@
 package quiz
+
+import (
+	"net/http"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/quiz_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func GetAll(c *gin.Context) {
+	userID, _ := c.Get("user_id")
+
+	quizzes, err := quiz_migrations.GetAll(userID.(uint))
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	r := response.NewSuccess(quizzes)
+	c.JSON(http.StatusOK, r)
+}
