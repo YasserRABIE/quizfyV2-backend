d8321fd113e32ed80bee906c7a02085ddadcebcd
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-18 12:00:44 +0300

    Add create and get functions for quiz migrations

--- quizfy-backend/migrations/quiz_migrations/create.go
+++ quizfy-backend/migrations/quiz_migrations/create.go
@@ -1,1 +1,15 @@
 package quiz_migrations
+
+import (
+	"errors"
+
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+)
+
+func Create(quizData *quiz.Quiz) error {
+	if err := db.Conn.Create(quizData).Error; err != nil {
+		return errors.New("لم يتم إنشاء الاختبار بنجاح!، يرجى المحاولة مرة أخرى")
+	}
+	return nil
+}
