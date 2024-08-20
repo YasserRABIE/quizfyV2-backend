d0ec5bcdbebea679efc2b01b107cbb56e0053534
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-20 10:53:16 +0300

    Update quiz update route and migration

--- quizfy-backend/controllers/quiz/update.go
+++ quizfy-backend/controllers/quiz/update.go
@@ -1,1 +1,43 @@
 package quiz
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/quiz_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Update(c *gin.Context) {
+	var quizData quiz.Quiz
+
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Bind request body
+	err = c.ShouldBindJSON(&quizData)
+	if err != nil {
+		err = errors.New("يرجى ملأ كل الحقول")
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Update quiz in database
+	err = quiz_migrations.Update(uint(idInt), &quizData)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Send success response
+	r := response.NewSuccess("تم تحديث الاختبار بنجاح")
+	c.JSON(http.StatusOK, r)
+}
--- quizfy-backend/migrations/quiz_migrations/update.go
+++ quizfy-backend/migrations/quiz_migrations/update.go
@@ -1,1 +1,25 @@
 package quiz_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+)
+
+func Update(id uint, quizData *quiz.Quiz) error {
+	err := db.Conn.Model(&quiz.Quiz{}).Where("id = ?", id).Updates(
+		quiz.Quiz{
+			Title:       quizData.Title,
+			Description: quizData.Description,
+			Difficulty:  quizData.Difficulty,
+			Type:        quizData.Type,
+			IsTimeBased: quizData.IsTimeBased,
+			Duration:    quizData.Duration,
+			OpensAt:     quizData.OpensAt,
+			ClosesAt:    quizData.ClosesAt,
+		},
+	).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
