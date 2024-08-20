a6ce717c7033333713b03addf789fac9a0a03d5b
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-20 11:22:58 +0300

    Add delete functionality to quiz

--- quizfy-backend/controllers/quiz/delete.go
+++ quizfy-backend/controllers/quiz/delete.go
@@ -1,1 +1,29 @@
 package quiz
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/quiz_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Delete(c *gin.Context) {
+	id := c.Param("id")
+	idInt, err := strconv.Atoi(id)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	err = quiz_migrations.DeleteByID(uint(idInt))
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	r := response.NewSuccess("تم حذف الاختبار بنجاح")
+	c.JSON(http.StatusOK, r)
+}
--- quizfy-backend/migrations/quiz_migrations/delete.go
+++ quizfy-backend/migrations/quiz_migrations/delete.go
@@ -1,1 +1,14 @@
 package quiz_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+)
+
+func DeleteByID(id uint) error {
+	err := db.Conn.Delete(&quiz.Quiz{}, id).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
