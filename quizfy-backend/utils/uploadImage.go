0c9474cabb89a8a8cd2a3fb4302c5307537ab33c
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-22 00:18:02 +0300

    Refactor image upload function to handle file size limit

--- quizfy-backend/utils/uploadImage.go
+++ quizfy-backend/utils/uploadImage.go
@@ -5,31 +5,40 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+
+	"github.com/YasserRABIE/QUIZFYv2/config"
 )
 
-const (
-	BasePath = "uploads/quiz/"
+var (
+	BasePath = getBasePath()
 )
+
+func getBasePath() string {
+	return config.GetEnv("QUIZ_UPLOAD_PATH", "uploads/quiz/")
+}
 
 // UploadImage uploads an image to the server and returns the path to the image
-func UploadImage(imageBase64 string, extension string, quizID, questionID uint) string {
+func UploadImage(imageBase64 string, extension string, quizID, questionID uint) (string, error) {
+	// Decode the base64 image data
 	imageData, err := base64.StdEncoding.DecodeString(imageBase64)
 	if err != nil {
-		fmt.Println("Error decoding base64 string:", err)
-		return ""
+		return "", err
+	}
+
+	// Check if the image size is too large (e.g., more than 5MB)
+	if len(imageData) > 5*1024*1024 {
+		return "", fmt.Errorf("file size exceeds limit")
 	}
 
 	// Ensure the base path exists
-	if err := os.MkdirAll(BasePath, os.ModePerm); err != nil {
-		fmt.Println("Error creating base path:", err)
-		return ""
+	if err := os.MkdirAll(BasePath, 0755); err != nil {
+		return "", err
 	}
 
 	// Ensure the quiz directory exists
 	quizPath := filepath.Join(BasePath, fmt.Sprintf("%d", quizID))
-	if err := os.MkdirAll(quizPath, os.ModePerm); err != nil {
-		fmt.Println("Error creating quiz directory:", err)
-		return ""
+	if err := os.MkdirAll(quizPath, 0755); err != nil {
+		return "", err
 	}
 
 	fileName := fmt.Sprintf("question_%d.%s", questionID, extension)
@@ -38,9 +47,8 @@ func UploadImage(imageBase64 string, extension string, quizID, questionID uint) 
 	// Write the image data to the file
 	err = os.WriteFile(destPath, imageData, 0644)
 	if err != nil {
-		fmt.Println("Error writing file:", err)
-		return ""
+		return "", err
 	}
 
-	return destPath
+	return destPath, nil
 }
