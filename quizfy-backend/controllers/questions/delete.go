61b3fb6f4ef79d146510563b89be8062dc35bfaa
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-21 20:19:15 +0300

    Add delete functionality for questions

--- quizfy-backend/controllers/questions/delete.go
+++ quizfy-backend/controllers/questions/delete.go
@@ -1,1 +1,38 @@
 package question
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+
+	question_migrations "github.com/YasserRABIE/QUIZFYv2/migrations/questions_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Delete(c *gin.Context) {
+	questionID := c.Param("question_id")
+	questionIDInt, err := strconv.Atoi(questionID)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	imagePath, err := question_migrations.Delete(uint(questionIDInt))
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+	// delete question image if exists
+	if imagePath != "" {
+		err = os.Remove(imagePath)
+		if err != nil {
+			utils.HandleError(c, err, http.StatusBadRequest)
+			return
+		}
+	}
+
+	r := response.NewSuccess("تم حذف السؤال بنجاح")
+	c.JSON(http.StatusOK, r)
+}
--- quizfy-backend/migrations/questions_migrations/delete.go
+++ quizfy-backend/migrations/questions_migrations/delete.go
@@ -1,1 +1,30 @@
 package question_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+)
+
+func Delete(id uint) (string, error) {
+	var imagePath string
+
+	err := db.Conn.Model(&quiz.Question{}).
+		Where("id = ?", id).
+		Pluck("image_path", &imagePath).Error
+
+	if err != nil {
+		return "", err
+	}
+
+	err = db.Conn.Where("question_id = ?", id).Delete(&quiz.Option{}).Error
+	if err != nil {
+		return "", err
+	}
+
+	err = db.Conn.Delete(&quiz.Question{}, id).Error
+	if err != nil {
+		return "", err
+	}
+
+	return imagePath, nil
+}
