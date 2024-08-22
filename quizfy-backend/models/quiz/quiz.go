4fe9ab8d18dbd0dde6be8ce1578f2274288dd1dd
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-22 19:12:52 +0300

    Refactor question and quiz models

--- quizfy-backend/models/quiz/quiz.go
+++ quizfy-backend/models/quiz/quiz.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Quiz struct {
 	gorm.Model
-	UserID      uint   `json:"user_id" gorm:"not null"`
+	UserID uint `json:"user_id" gorm:"not null"`
 
 	Title       string `json:"title" gorm:"not null" binding:"required"`
 	Description string `json:"description" gorm:"not null" binding:"required"`
@@ -14,6 +14,26 @@ type Quiz struct {
 	Duration    *int   `json:"duration" binding:"required"`
 	OpensAt     string `json:"opens_at" gorm:"not null" binding:"required"`
 	ClosesAt    string `json:"closes_at" gorm:"not null" binding:"required"`
+}
+
+// BeforeDelete hook deletes all questions related to the quiz
+func (q *Quiz) BeforeDelete(tx *gorm.DB) (err error) {
+	var questionIDs []uint
+
+	// Retrieve all question IDs associated with this quiz
+	// so we can delete them and their options in a single transaction
+	if err := tx.Model(&Question{}).Where("quiz_id = ?", q.ID).Pluck("id", &questionIDs).Error; err != nil {
+		return err
+	}
+
+	// Delete all questions associated with this quiz and their options will be deleted automatically
+	for _, id := range questionIDs {
+		if err := tx.Delete(&Question{Model: gorm.Model{ID: id}}).Error; err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 // TableName sets the table name for the Quiz model
