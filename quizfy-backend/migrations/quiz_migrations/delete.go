4f3df6569a05eb36d6c4c9b2c48ef1b0c07688cd
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-22 19:12:40 +0300

    Fix delete function in quiz migrations

--- quizfy-backend/migrations/quiz_migrations/delete.go
+++ quizfy-backend/migrations/quiz_migrations/delete.go
@@ -3,10 +3,11 @@ package quiz_migrations
 import (
 	"github.com/YasserRABIE/QUIZFYv2/db"
 	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+	"gorm.io/gorm"
 )
 
 func DeleteByID(id uint) error {
-	err := db.Conn.Delete(&quiz.Quiz{}, id).Error
+	err := db.Conn.Delete(&quiz.Quiz{Model: gorm.Model{ID: id}}).Error
 	if err != nil {
 		return err
 	}
