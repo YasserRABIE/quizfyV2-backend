b86600fb9d87d31bc565854748d1a878d680f987
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-23 22:06:56 +0300

    Add session creation functionality

--- quizfy-backend/controllers/session/create.go
+++ quizfy-backend/controllers/session/create.go
@@ -1,1 +1,33 @@
 package session
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/session_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Create(c *gin.Context) {
+	userIdAny, _ := c.Get("user_id")
+	userID := userIdAny.(uint)
+	quizID := c.Param("quiz_id")
+	quizIDInt, err := strconv.Atoi(quizID)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	// Create a new session
+	session, err := session_migrations.Create(uint(quizIDInt), userID)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	// Send success response
+	r := response.NewSuccess(session)
+	c.JSON(http.StatusCreated, r)
+}
--- quizfy-backend/migrations/session_migrations/create.go
+++ quizfy-backend/migrations/session_migrations/create.go
@@ -1,1 +1,17 @@
 package session_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/session"
+)
+
+func Create(quizID, userID uint) (*session.Session, error) {
+	session := &session.Session{
+		QuizID: quizID,
+		UserID: userID,
+	}
+	if err := db.Conn.Create(session).Error; err != nil {
+		return nil, err
+	}
+	return session, nil
+}
