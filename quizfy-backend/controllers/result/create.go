d5dd3b43352d0f62f2542d3f9c9a0b3a891ac42f
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-25 19:08:23 +0300

    Add result creation endpoint and migration

--- quizfy-backend/controllers/result/create.go
+++ quizfy-backend/controllers/result/create.go
@@ -1,1 +1,48 @@
 package result
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/result_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/models/result"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Create(c *gin.Context) {
+	var result result.Result
+
+	sessionID, quizID, err := getIdsAsUint(c)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+	if err := c.ShouldBindJSON(&result.ReviewedAnswers); err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+	err = result_migrations.Create(sessionID, quizID, &result)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusInternalServerError)
+		return
+	}
+
+	r := response.NewSuccess(result)
+	c.JSON(http.StatusCreated, r)
+}
+
+func getIdsAsUint(c *gin.Context) (uint, uint, error) {
+	sessionID := c.Param("session_id")
+	quizID := c.Param("quiz_id")
+	sessionIDInt, err := strconv.Atoi(sessionID)
+	if err != nil {
+		return 0, 0, err
+	}
+	quizIDInt, err := strconv.Atoi(quizID)
+	if err != nil {
+		return 0, 0, err
+	}
+	return uint(sessionIDInt), uint(quizIDInt), nil
+}
--- quizfy-backend/migrations/result_migrations/create.go
+++ quizfy-backend/migrations/result_migrations/create.go
@@ -1,1 +1,49 @@
 package result_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	question_migrations "github.com/YasserRABIE/QUIZFYv2/migrations/questions_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
+	"github.com/YasserRABIE/QUIZFYv2/models/result"
+)
+
+func Create(sessionID, quizID uint, result *result.Result) error {
+	for i := range result.ReviewedAnswers {
+		answer := &result.ReviewedAnswers[i] 
+
+		var err error
+		answer.Question, err = question_migrations.GetByID(answer.QuestionID)
+		if err != nil {
+			return err
+		}
+		answer.SessionID = sessionID
+
+		// validate the answer
+		switch answer.Question.Type {
+		case "MCQ":
+			answer.IsCorrect = getCorrectOption(answer.Question.Options) == answer.UserAnswer.OptionID
+			updateResultCounts(answer.IsCorrect, result, answer.Question)
+		case "BOOL":
+			answer.IsCorrect = answer.Question.BoolAnswer == answer.UserAnswer.BoolAnswer
+			updateResultCounts(answer.IsCorrect, result, answer.Question)
+		}
+	}
+	return db.Conn.Create(result).Error
+}
+
+func updateResultCounts(isCorrect bool, result *result.Result, question *quiz.Question) {
+	if isCorrect {
+		result.CorrectCount++
+		result.Score += question.Degree
+	}
+	result.Total += question.Degree
+}
+
+func getCorrectOption(options []quiz.Option) uint {
+	for _, option := range options {
+		if option.IsCorrect {
+			return option.ID
+		}
+	}
+	return 0
+}
