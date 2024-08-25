441fbbc021120f8215432508ec54a7cbf684021e
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-25 19:40:49 +0300

    Update import path for result package

--- quizfy-backend/migrations/result_migrations/get.go
+++ quizfy-backend/migrations/result_migrations/get.go
@@ -2,11 +2,11 @@ package result_migrations
 
 import (
 	"github.com/YasserRABIE/QUIZFYv2/db"
-	"github.com/YasserRABIE/QUIZFYv2/models/result"
+	"github.com/YasserRABIE/QUIZFYv2/models/quiz"
 )
 
-func Get(sessionID uint) (*result.Result, error) {
-	var result result.Result
+func Get(sessionID uint) (*quiz.Result, error) {
+	var result quiz.Result
 	err := db.Conn.Where("session_id = ?", sessionID).First(&result).Error
 	if err != nil {
 		return nil, err
