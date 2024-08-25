daed15896c0b3d91bfc1e0f011d8342114667f05
YasserRABIE <yasserrabie110306@gmail.com>
2024-08-25 19:08:32 +0300

    Add result get endpoint

--- quizfy-backend/controllers/result/get.go
+++ quizfy-backend/controllers/result/get.go
@@ -1,1 +1,29 @@
 package result
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/YasserRABIE/QUIZFYv2/migrations/result_migrations"
+	"github.com/YasserRABIE/QUIZFYv2/models/response"
+	"github.com/YasserRABIE/QUIZFYv2/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Get(c *gin.Context) {
+	sessionID := c.Param("session_id")
+	sessionIDInt, err := strconv.Atoi(sessionID)
+	if err != nil {
+		utils.HandleError(c, err, http.StatusBadRequest)
+		return
+	}
+
+	result, err := result_migrations.Get(uint(sessionIDInt))
+	if err != nil {
+		utils.HandleError(c, err, http.StatusNotFound)
+		return
+	}
+
+	r := response.NewSuccess(result)
+	c.JSON(http.StatusOK, r)
+}
--- quizfy-backend/migrations/result_migrations/get.go
+++ quizfy-backend/migrations/result_migrations/get.go
@@ -1,1 +1,15 @@
 package result_migrations
+
+import (
+	"github.com/YasserRABIE/QUIZFYv2/db"
+	"github.com/YasserRABIE/QUIZFYv2/models/result"
+)
+
+func Get(sessionID uint) (*result.Result, error) {
+	var result result.Result
+	err := db.Conn.Where("session_id = ?", sessionID).First(&result).Error
+	if err != nil {
+		return nil, err
+	}
+	return &result, err
+}
